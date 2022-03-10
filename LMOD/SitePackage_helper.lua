7738fa99ca232d43fe7bcb73b003eec18c45e319
Kurt Lust <Kurt.Lust@uantwerpen.be>
2022-03-10 18:40:47 +0100

    Additional helper function: mkDir.

--- LMOD/SitePackage_helper.lua
+++ LMOD/SitePackage_helper.lua
@@ -1,16 +1,16 @@
 -- -----------------------------------------------------------------------------
--- 
+--
 -- Helper functions and data structures for SitePackage meant
 -- to avoid recomputing stuff too often during a single call
 -- to Lmod.
 --
 
 --
--- Build a table with the keys from CalcUA_map_arch_hierarchy sorted 
+-- Build a table with the keys from CalcUA_map_arch_hierarchy sorted
 --
 CalcUA_sorted_archmap_keys = {}
 
-for key in pairs( CalcUA_map_arch_hierarchy ) 
+for key in pairs( CalcUA_map_arch_hierarchy )
 do
     table.insert( CalcUA_sorted_archmap_keys, key )
 end
@@ -31,6 +31,49 @@ function get_matching_key( version )
     end
 
     return CalcUA_sorted_archmap_keys[index]
+
+end
+
+
+-- -----------------------------------------------------------------------------
+--
+-- mkDir( dirname )
+--
+-- Helper function to create a directory if the directory does not already exist
+--
+
+function mkDir( dirname )
+
+    local lfs = require( 'lfs' )
+
+    local build_dir = ''
+    if dirname:sub( 1, 1 ) == '/' then
+        build_dir = '/'
+    end
+
+    for str in dirname:split( '/' ) do
+
+        if str ~= '' then
+
+            build_dir = pathJoin( build_dir, str )
+            -- print( build_dir )
+
+            if not isDir( build_dir ) then
+
+                -- print( build_dir .. ' not found, creating...' )
+
+                if not lfs.mkdir( build_dir ) then
+                    io.stderr:write( 'ERROR: Failed to create the directory ' .. build_dir )
+                    return false
+                end
+
+            end
+
+        end
+
+    end
+
+    return true
 
 end
 
