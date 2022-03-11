ad42fe5474110bc823b1a312d5da99cd1ad84343
Kurt Lust <Kurt.Lust@uantwerpen.be>
2022-03-11 18:19:37 +0100

    Further work on the software stack creation routine in Lua.

--- scripts/calcua_tools/create_calcua_stack_dirs.lua
+++ scripts/calcua_tools/create_calcua_stack_dirs.lua
@@ -6,16 +6,16 @@ dofile( '../../LMOD/SitePackage_helper.lua' )
 dofile( '../../LMOD/SitePackage_map_toolchain.lua' )
 dofile( '../../LMOD/SitePackage_arch_hierarchy.lua' )
 
-routine_name = 'create_calcua_stack_dirs'
-stack_name = 'calcua'
+local routine_name = 'create_calcua_stack_dirs'
+local stack_name = 'calcua'
 
 if #arg ~= 2 then
     io.stderr:write( routine_name .. ': ERROR: Two command line argument is expected: the version of the calcua stack and the root of the installation.\n' )
     os.exit( 1 )
 end
 
-stack_version = arg[1]
-root_dir = arg[2]
+local stack_version = arg[1]
+local root_dir = arg[2]
 
 if CalcUA_SystemTable[stack_version] == nil then
     io.stderr:write( routine_name .. ': ERROR: The stack version ' .. stack_version .. ' is not recognized as a valid stack.\n' ..
@@ -29,8 +29,8 @@ end
 
 print( 'Computing all OS-arch combinations needed for ' .. stack_name .. '/' .. stack_version .. '...' )
 
-OSArchTable = {}
-OSArchTableWorker = {}
+local OSArchTable = {}
+local OSArchTableWorker = {}
 
 for OS,_ in pairs( CalcUA_SystemTable[stack_version] ) do
 
@@ -55,7 +55,8 @@ print( 'Detected the following OS-arch combination:\n' .. table.concat( OSArchTa
 --
 -- Create directories
 --
-
+-- -   First the directories that EasyBuild will write in
+--
 
 for _,longname in ipairs( OSArchTable ) do
 
@@ -78,3 +79,45 @@ for _,longname in ipairs( OSArchTable ) do
     mkDir( EBrepo_dir )
 
 end
+
+--
+-- -   Now the directories for the actual modules
+--
+--     *   Software stack module
+--
+
+local stack_dir = pathJoin( root_dir, 'modules-infrastructure/stack/calcua' )
+mkDir( stack_dir )
+
+-- TODO: Link to the right version of UAntwerpen-modules/generic-modules/calcua
+
+--
+--    *    Architecture modules
+--
+
+local arch_dir = pathJoin( root_dir, 'modules-infrastructure/arch/calcua', stack_version )
+
+mkDir( pathJoin( arch_dir, 'arch' ) )
+
+for _,longname in ipairs( OSArchTable ) do
+
+    local link_name = pathJoin( arch_dir, 'arch', longname )
+    -- TODO: Link the module to the right version of UAntwerpen-modules/generic-modules/clusterarch
+
+end
+
+--
+--    *    Cluster modules (architecture modules with cluster name)
+--
+
+local arch_dir = pathJoin( root_dir, 'modules-infrastructure/arch/calcua', stack_version )
+
+mkDir( pathJoin( arch_dir, 'cluster' ) )
+
+for cluster,_ in ipairs( CalcUA_ClusterMap[stack_version] ) do
+
+    local link_name = pathJoin( arch_dir, 'cluster', cluster )
+    -- TODO: Link the module to the right version of UAntwerpen-modules/generic-modules/clusterarch
+
+end
+
