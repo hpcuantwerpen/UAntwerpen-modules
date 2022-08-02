e5ae37120729c78b93536cdf78fce38eef773678
Kurt Lust <Kurt.Lust@uantwerpen.be>
2022-08-02 18:21:29 +0200

    Minor correction in the text of an error message.

--- scripts/ClusterMod_tools/helper_get_stack_genericarch_current.lua
+++ scripts/ClusterMod_tools/helper_get_stack_genericarch_current.lua
@@ -5,7 +5,7 @@ local lfs = require( 'lfs' )
 local routine_name = 'helper_get_stack_genericarch_current'
 
 if #arg ~= 1 then
-    io.stderr:write( routine_name .. ': ERROR: One command line argument is expected: the version of the calcua stack.\n' )
+    io.stderr:write( routine_name .. ': ERROR: One command line argument is expected: the version of the stack.\n' )
     os.exit( 1 )
 end
 
